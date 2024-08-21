e886aebb0afabc0d94c289918efc7d3432c8fb84
ththory <jming2415@gmail.com>
2024-08-21 17:59:22 +0900

    add: your-first-component

---
+++ src/your-first-component/Profile.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+    return (
+        <div>
+            <img
+                src="https://i.imgur.com/MK3eW3As.jpg"
+                alt="Katherine Johnson"
+            />
+        </div>
+    );
+}
