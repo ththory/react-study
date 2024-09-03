3fca4b6521a851e543f6481908c1e4dc5404bcf2
ththory <jming2415@gmail.com>
2024-09-03 16:23:16 +0900

    Add: reacting-to-input-with-state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Form from "./reacting-to-input-with-state/Form";
 import BucketList from "./updating-arrays-in-state/BucketList";
 import List from "./updating-arrays-in-state/List";
 function App() {
     return (
         <section>
-            <BucketList></BucketList>
+            <Form></Form>
         </section>
     );
 }
