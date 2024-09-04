6a06adb94958c3f4353f9e9b3486702466d8a28f
ththory <jming2415@gmail.com>
2024-09-04 14:00:24 +0900

    Add: State 구조 선택하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import FeedbackForm from "./choosing-the-state-structure/FeedbackForm";
 import Form from "./reacting-to-input-with-state/Form";
 import BucketList from "./updating-arrays-in-state/BucketList";
 import List from "./updating-arrays-in-state/List";
 function App() {
     return (
         <section>
-            <Form></Form>
+            <FeedbackForm></FeedbackForm>
         </section>
     );
 }
